bf0e5e078f6f9f5fd2ad5326605c958457aab422
Jordan Austin <jordotech@gmail.com>
2017-01-07 14:21:30 -0600

    thumbs, more css, ful res link

--- app.js
+++ app.js
@@ -3,24 +3,28 @@ var m = {
     "images": [
 
         {
+            "thumb":"./media/THUMBS/Beck_01.jpg",
             "src":"./media/IMAGES/Beck_01.jpg",
             "title":"Untitled, 2016",
             "description": "Ink, watercolor, pencil, and powdered charcoal on paper",
             "dimensions":"30 x 35 inches",
         },
         {
+            "thumb":"./media/THUMBS/Beck_02.jpg",
             "src":"./media/IMAGES/Beck_02.jpg",
             "title":"Untitled, 2016",
             "description": "Watercolor, powdered charcoal, ink and loose pigment on paper",
             "dimensions":"30 x 33 inches",
         },
         {
+            "thumb":"./media/THUMBS/Beck_03.jpg",
             "src":"./media/IMAGES/Beck_03.jpg",
             "title":"Untitled, 2015",
             "description": "Watercolor and ink on paper",
             "dimensions":"30 x 22.5 inches",
         },
         {
+            "thumb":"./media/THUMBS/Beck_04.jpg",
             "src":"./media/IMAGES/Beck_04.jpg",
             "title":"Untitled, 2012",
             "description": "Acrylic, ink, collage, pastel and synthetic hair on paper",
@@ -29,24 +33,28 @@ var m = {
 
 
         {
+            "thumb":"./media/THUMBS/Beck_05.jpg",
             "src":"./media/IMAGES/Beck_05.jpg",
             "title":"Installation view, \"Drone\", Logan Center for the Arts, Chicago, IL, 2012",
             "description": "",
             "dimensions":"",
         },
         {
+            "thumb":"./media/THUMBS/Beck_06.jpg",
             "src":"./media/IMAGES/Beck_06.jpg",
             "title":"Panorama, 2015",
             "description": "Ink, watercolor, pencil and gouache on paper",
             "dimensions":"20.5 x 25 inches",
         },
         {
+            "thumb":"./media/THUMBS/Beck_07.jpg",
             "src":"./media/IMAGES/Beck_07.jpg",
             "title":"Man Moth, 2013",
             "description": "Pencil, pastel, watercolor, pigment and collage on paper",
             "dimensions":"35 x 27.5 inches",
         },
         {
+            "thumb":"./media/THUMBS/Beck_08.jpg",
             "src":"./media/IMAGES/Beck_08.jpg",
             "title":"Islands in the Net, 2014",
             "description": "Ink, pastel, acrylic and collage on paper",
@@ -55,24 +63,28 @@ var m = {
 
 
         {
+            "thumb":"./media/THUMBS/Beck_09.jpg",
             "src":"./media/IMAGES/Beck_09.jpg",
             "title":"Untitled, 2015",
             "description": "Watercolor on paper",
             "dimensions":"25.5 x 27.5 inches",
         },
         {
+            "thumb":"./media/THUMBS/Beck_10.jpg",
             "src":"./media/IMAGES/Beck_10.jpg",
             "title":"Untitled, 2013",
             "description": "Ink, watercolor, pencil, cork and collage on paper",
             "dimensions":"10 x 13 inches",
         },
         {
+            "thumb":"./media/THUMBS/Beck_11.jpg",
             "src":"./media/IMAGES/Beck_11.jpg",
             "title":"Mountaineer, 2014",
             "description": "Ink, watercolor and pastel on paper",
             "dimensions":"34 inch diameter",
         },
         {
+            "thumb":"./media/THUMBS/Beck_12.jpg",
             "src":"./media/IMAGES/Beck_12.jpg",
             "title":"Untitled, 2016",
             "description": "Ink, pastel and watercolor on paper",
@@ -81,18 +93,21 @@ var m = {
 
 
         {
+            "thumb":"./media/THUMBS/Beck_13.jpg",
             "src":"./media/IMAGES/Beck_13.jpg",
             "title":"Pleasure of Ruins, 2015",
             "description": "Ink, watercolor, acrylic, pencil, gouache and collage on paper",
             "dimensions":"26 x 24 inches",
         },
         {
+            "thumb":"./media/THUMBS/Beck_14.jpg",
             "src":"./media/IMAGES/Beck_14.jpg",
             "title":"Sacrifice, 2015",
             "description": "Ink, acrylic, watercolor and pencil on paper",
             "dimensions":"12 x 9 inches",
         },
         {
+            "thumb":"./media/THUMBS/Beck_15.jpg",
             "src":"./media/IMAGES/Beck_15.jpg",
             "title":"Untitled, 2015",
             "description": "Oil on panel",
@@ -101,54 +116,63 @@ var m = {
     ],
     "stills": [
         {
+            "thumb":"./media/THUMBS/Beck_16.jpg",
             "src":"./media/IMAGES/Beck_16.jpg",
             "title":"Still, Delusions of Grandeur, 2016",
             "description": "HD video, 12:04",
             "dimensions":"",
         },
         {
+            "thumb":"./media/THUMBS/Beck_17.jpg",
             "src":"./media/IMAGES/Beck_17.jpg",
             "title":"Still, Delusions of Grandeur, 2016",
             "description": "HD video, 12:04",
             "dimensions":"",
         },
         {
+            "thumb":"./media/THUMBS/Beck_18.jpg",
             "src":"./media/IMAGES/Beck_18.jpg",
             "title":"Still, Delusions of Grandeur, 2016",
             "description": "HD video, 12:04",
             "dimensions":"",
         },
         {
+            "thumb":"./media/THUMBS/Beck_19.jpg",
             "src":"./media/IMAGES/Beck_19.jpg",
             "title":"Still, Delusions of Grandeur, 2016",
             "description": "HD video, 12:04",
             "dimensions":"",
         },
         {
+            "thumb":"./media/THUMBS/Beck_20.jpg",
             "src":"./media/IMAGES/Beck_20.jpg",
             "title":"Still, Delusions of Grandeur, 2016",
             "description": "HD video, 12:04",
             "dimensions":"",
         },
         {
+            "thumb":"./media/THUMBS/Beck_21.jpg",
             "src":"./media/IMAGES/Beck_21.jpg",
             "title":"Still, Delusions of Grandeur, 2016",
             "description": "HD video, 12:04",
             "dimensions":"",
         },
         {
+            "thumb":"./media/THUMBS/Beck_22.jpg",
             "src":"./media/IMAGES/Beck_22.jpg",
             "title":"Still, Wide Sea, 2014",
             "description": "HD video, 7:41",
             "dimensions":"",
         },
         {
+            "thumb":"./media/THUMBS/Beck_23.jpg",
             "src":"./media/IMAGES/Beck_23.jpg",
             "title":"Still, Wide Sea, 2014",
             "description": "HD video, 7:41",
             "dimensions":"",
         },
         {
+            "thumb":"./media/THUMBS/Beck_24.jpg",
             "src":"./media/IMAGES/Beck_24.jpg",
             "title":"Still, A Modern Utopia, 2013",
             "description": "HD video, 6:32",
@@ -208,6 +232,7 @@ $(function () {
         $(this).find('.modal-body').html($(source).find('.panel-body').html());
         $(this).find('.modal-title h4').html($(source).find('.panel-footer .title').html());
         $(this).find('.modal-title .description').html($(source).find('.panel-footer .description').html());
+        $(this).find('.modal-title .full-res-link').html($(source).find('.panel-footer .full-res-link').html());
 
     });
 })
