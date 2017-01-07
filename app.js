90e2cd3a64765992ee653231ec9dd68bc6e6513e
Jordan Austin <jordotech@gmail.com>
2017-01-07 10:40:04 -0600

    now using json and ng-repeat

--- app.js
+++ app.js
@@ -16,8 +16,18 @@ animateApp.config(function ($routeProvider) {
         });
 
 });
-
+var m = {
+    "images": [
+        {
+            "src":"./media/IMAGES/Beck_01.jpg",
+            "title":"Untitled, 2016",
+            "description": "Ink, watercolor, pencil, and powdered charcoal on paper",
+            "dimensions":"30 x 35 inches",
+        }
+    ]
+};
 animateApp.controller('mainController', function ($scope) {
+    $scope.items = m.images;
     $scope.pageClass = 'page-home';
 });
 
@@ -29,4 +39,14 @@ animateApp.controller('contactController', function ($scope) {
     $scope.pageClass = 'page-contact';
 });
 
+$(function () {
+    $('#myModal').on('show.bs.modal', function (event) {
+        var source = $(event.relatedTarget);
+
+        $(this).find('.modal-body').html($(source).find('.panel-body').html());
+        $(this).find('.modal-title h4').html($(source).find('.panel-footer .title').html());
+        $(this).find('.modal-title .description').html($(source).find('.panel-footer .description').html());
+
+    });
+})
 
