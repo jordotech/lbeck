308a4c3d25fdb9e9d996dd5324ad1e13b7ae29bb
Jordan Austin <jordotech@gmail.com>
2017-01-07 10:04:58 -0600

    bootstrap modals working

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
 
-animateApp.config(function($routeProvider) {
+animateApp.config(function ($routeProvider) {
     $routeProvider
-    	.when('/', {
-    		templateUrl: 'page-home.html',
+        .when('/', {
+            templateUrl: 'page-home.html',
             controller: 'mainController'
-    	})
-    	.when('/images', {
-    		templateUrl: 'page-images.html',
+        })
+        .when('/images', {
+            templateUrl: 'page-images.html',
             controller: 'imagesController'
-    	})
-    	.when('/contact', {
-    		templateUrl: 'page-contact.html',
+        })
+        .when('/contact', {
+            templateUrl: 'page-contact.html',
             controller: 'contactController'
-    	});
+        });
 
 });
 
-animateApp.controller('mainController', function($scope) {
+animateApp.controller('mainController', function ($scope) {
     $scope.pageClass = 'page-home';
 });
 
-animateApp.controller('imagesController', function($scope) {
+animateApp.controller('imagesController', function ($scope) {
     $scope.pageClass = 'page-images';
 });
 
-animateApp.controller('contactController', function($scope) {
+animateApp.controller('contactController', function ($scope) {
     $scope.pageClass = 'page-contact';
 });
+
+
